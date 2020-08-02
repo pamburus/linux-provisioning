a7381ebf73a7b1122398f198b8981c24ea46d8c3
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2020-08-02 23:47:20 +0300

    new: improved collect.sh

--- collect.sh
+++ collect.sh
@@ -4,11 +4,33 @@ set -e
 # target locations
 target=$(dirname "$0")/data
 
+include_configs=false
+
+function usage() {
+	echo "$0 [--include-configs]"
+}
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-c | --include-configs )
+			include_configs=true
+			;;
+		-h | --help )
+			usage
+			exit
+			;;
+        * )
+			usage
+			exit 1
+	esac
+	shift
+done
+
 # source files
 bat=https://github.com/sharkdp/bat/releases/download/v0.15.4/bat-v0.15.4-x86_64-unknown-linux-musl.tar.gz
 lsd=https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd-0.17.0-x86_64-unknown-linux-musl.tar.gz
 procs=https://github.com/dalance/procs/releases/download/v0.10.3/procs-0.10.3-1.x86_64.rpm
-hl=~/bin/linux/hl
+hl=https://github.com/pamburus/hl/releases/download/v0.1.3/hl-linux
 vimrc=~/.vim/vimrc
 tmuxconf=~/.tmux.conf
 key=~/.ssh/id_rsa.pub
@@ -28,13 +50,17 @@ function git-clone() {
 
 # actions
 mkdir -p "${target:?}"/{etc/micro,share,keys,bin,dist,src}
-! test -f "${vimrc:?}" || cp "${vimrc:?}" "${target:?}"/etc/vim/
-! test -f "${tmuxconf:?}" || cp "${tmuxconf:?}" "${target:?}"/etc/tmux/
-! test -d "${microcfg:?}" || cp "${microcfg:?}"/*.json "${target:?}"/etc/micro/
-! test -f "${key:?}" || cp "${key:?}" "${target:?}"/keys/current.key
-! test -f "${hl:?}" || cp "${hl:?}" "${target:?}"/bin/
-! test -d "${alacrittycfg:?}" || cp "${alacrittycfg:?}"/* "${target:?}"/etc/alacritty/
+if [ "${include_configs:?}" == true ]; then
+	! test -f "${vimrc:?}" || cp "${vimrc:?}" "${target:?}"/etc/vim/
+	! test -f "${tmuxconf:?}" || cp "${tmuxconf:?}" "${target:?}"/etc/tmux/
+	! test -d "${microcfg:?}" || cp "${microcfg:?}"/*.json "${target:?}"/etc/micro/
+	! test -f "${key:?}" || cp "${key:?}" "${target:?}"/keys/current.key
+	! test -f "${hl:?}" || cp "${hl:?}" "${target:?}"/bin/
+	! test -d "${alacrittycfg:?}" || cp "${alacrittycfg:?}"/* "${target:?}"/etc/alacritty/
+fi
 git-clone https://github.com/tmux-plugins/tpm ${target:?}/etc/tmux/.tmux/plugins/tpm
 git-clone https://github.com/jimeh/tmux-themepack.git ${target:?}/etc/tmux/.tmux-themepack
 tar -C "${target:?}"/etc/tmux -cz -f "${target:?}"/share/tmux.tar.gz ${tmux:?}
 wget -N -q -P "${target:?}"/dist/ ${bat:?} ${lsd:?} ${procs:?}
+wget -N -q -O "${target:?}"/bin/hl ${hl:?}
+chmod +x "${target:?}"/bin/hl
