c4e6308414ca33fc230d8ffbe6aacc1f1a7abf2c
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2020-08-21 15:14:17 +0300

    new: install all components to homedir

--- apply.sh
+++ apply.sh
@@ -3,25 +3,21 @@ set -ex
 
 # source location
 source=$(realpath $(dirname $0)/data)
-target=/usr/local/sbin
-
-function install {
-	sudo yum -q list installed "$@" || sudo yum -q -y install "$@"  
-}
+target=~/bin
 
 # actions
-mkdir -p ~/.vim ~/.config
+mkdir -p "${target:?}" ~/.vim ~/.config
 cp "${source:?}"/etc/vim/vimrc ~/.vim/vimrc
 cp -r "${source:?}"/etc/micro ~/.config/
 tar -C ~ -x -f "${source:?}"/share/tmuxcfg.tar.gz
-install vim htop git
-sudo yum install -q -y "${source:?}"/dist/*.rpm || true
-sudo tar -C /usr/ -x --strip-components 1 -f "${source:?}"/dist/tmux.tar.gz
-sudo tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/bat.tar.gz '*/bat'
-sudo tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/lsd.tar.gz '*/lsd'
-sudo tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/delta.tar.gz '*/delta'
-sudo tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/micro.tar.gz '*/micro'
-sudo tar -C "${target:?}" -x -f "${source:?}"/dist/hl.tar.gz 'hl'
+sudo yum install -y -q vim htop git || true
+tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/tmux.tar.gz
+tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/bat.tar.gz '*/bat'
+tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/lsd.tar.gz '*/lsd'
+tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/delta.tar.gz '*/delta'
+tar -C "${target:?}" -x --strip-components 1 -f "${source:?}"/dist/micro.tar.gz '*/micro'
+unzip -f -d "${target:?}" "${source:?}"/dist/procs.zip
+tar -C "${target:?}" -x -f "${source:?}"/dist/hl.tar.gz 'hl'
 echo "source ${source:?}/scripts/profile.sh" | "${source:?}/scripts/append.sh" ~/.bash_profile
 for key in $(ls "${source:?}"/keys/*.key); do
 	cat ${key:?} | "${source:?}"/scripts/append.sh ~/.ssh/authorized_keys
--- collect.sh
+++ collect.sh
@@ -29,7 +29,7 @@ done
 # source files
 bat=https://github.com/sharkdp/bat/releases/download/v0.15.4/bat-v0.15.4-x86_64-unknown-linux-musl.tar.gz
 lsd=https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd-0.17.0-x86_64-unknown-linux-musl.tar.gz
-procs=https://github.com/dalance/procs/releases/download/v0.10.3/procs-0.10.3-1.x86_64.rpm
+procs=https://github.com/dalance/procs/releases/download/v0.10.4/procs-v0.10.4-x86_64-lnx.zip
 delta=https://github.com/dandavison/delta/releases/download/0.4.0/delta-0.4.0-x86_64-unknown-linux-musl.tar.gz
 hl=https://github.com/pamburus/hl/releases/download/v0.6.8/hl-linux.tar.gz
 micro=https://github.com/zyedidia/micro/releases/download/v2.0.6/micro-2.0.6-linux64-static.tar.gz
@@ -69,7 +69,7 @@ tar -C "${target:?}"/etc/tmux -cz -f "${target:?}"/share/tmuxcfg.tar.gz ${tmuxcf
 wget -N -q -O "${target:?}"/dist/tmux.tar.gz ${tmux:?}
 wget -N -q -O "${target:?}"/dist/bat.tar.gz ${bat:?} 
 wget -N -q -O "${target:?}"/dist/lsd.tar.gz ${lsd:?}
-wget -N -q -O "${target:?}"/dist/procs.rpm ${procs:?}
+wget -N -q -O "${target:?}"/dist/procs.zip ${procs:?}
 wget -N -q -O "${target:?}"/dist/delta.tar.gz ${delta:?}
 wget -N -q -O "${target:?}"/dist/micro.tar.gz ${micro:?}
 wget -N -q -O "${target:?}"/dist/hl.tar.gz ${hl:?}
