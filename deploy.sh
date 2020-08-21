a8e746571a76498d65098b3161d19f4e079024cb
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2020-08-21 15:34:51 +0300

    new: sudo option for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 set -e
 
+sudo=''
+
+function usage() {
+	echo "$0 [--su|-s]"
+}
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-s | --sudo )
+			sudo=sudo
+			;;
+		-h | --help )
+			usage
+			exit
+			;;
+        -* )
+			usage
+			exit 1
+			;;
+		* )
+			break
+			;;
+	esac
+	shift
+done
+
 for addr in "$@"; do
 	tar -C ~ -cz opt/provisioning | ssh ${addr:?} 'tar -C ~ -xz'
-	ssh ${addr:?} '~/opt/provisioning/apply.sh'
+	ssh ${addr:?} ${sudo} '~/opt/provisioning/apply.sh'
 done
