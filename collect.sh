8b841df53969c300641c43fb9dd9c2f2e8f98501
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2021-06-26 09:13:07 +0300

    fix: improved collect.sh script

--- collect.sh
+++ collect.sh
@@ -32,12 +32,12 @@ while [ "$1" != "" ]; do
 done
 
 # source files
-bat=https://github.com/sharkdp/bat/releases/download/v0.17.1/bat-v0.17.1-x86_64-unknown-linux-musl.tar.gz
-lsd=https://github.com/Peltoche/lsd/releases/download/0.19.0/lsd-0.19.0-x86_64-unknown-linux-musl.tar.gz
-procs=https://github.com/dalance/procs/releases/download/v0.10.10/procs-v0.10.10-x86_64-lnx.zip
-delta=https://github.com/dandavison/delta/releases/download/0.4.5/delta-0.4.5-x86_64-unknown-linux-musl.tar.gz
+bat=https://github.com/sharkdp/bat/releases/download/v0.18.1/bat-v0.18.1-x86_64-unknown-linux-musl.tar.gz
+lsd=https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd-0.20.1-x86_64-unknown-linux-musl.tar.gz
+procs=https://github.com/dalance/procs/releases/download/v0.11.9/procs-v0.11.9-x86_64-lnx.zip
+delta=https://github.com/dandavison/delta/releases/download/0.8.1/delta-0.8.1-x86_64-unknown-linux-musl.tar.gz
 hl=https://github.com/pamburus/hl/releases/download/v0.8.15/hl-linux.tar.gz
-micro=https://github.com/zyedidia/micro/releases/download/v2.0.8/micro-2.0.8-linux64-static.tar.gz
+micro=https://github.com/zyedidia/micro/releases/download/v2.0.9/micro-2.0.9-linux64-static.tar.gz
 tmux=https://github.com/owent-contrib/tmux-build-musl/releases/download/3.1b/tmux-3.1b.musl-bin.tar.gz
 vimrc=~/.vim/vimrc
 tmuxconf=~/.tmux.conf
@@ -49,11 +49,19 @@ tmuxcfg=".tmux.conf .tmux"
 function git-clone() {
 	local src="$1"
 	local dst="$2"
+	echo git clone "${src:?}"
 	if [ -d "${dst}" ]; then
 		git -C "${dst}" pull -q
 	else
 		git clone "${src}" "${dst}"
 	fi
+}
+
+function download() {
+	local src="$1"
+	local dst="$2"
+	echo download "${src:?}"
+	curl -sSfLR -o "${dst:?}" "${src:?}"
 }
 
 # actions
@@ -71,10 +79,10 @@ git-clone https://git::@github.com/tmux-plugins/tmux-sensible ${target:?}/etc/tm
 git-clone https://git::@github.com/jimeh/tmux-themepack.git ${target:?}/etc/tmux/.tmux/plugins/tmux-themepack
 git-clone https://git::@github.com/tmux-plugins/tpm ${target:?}/etc/tmux/.tmux/plugins/tpm
 tar -C "${target:?}"/etc/tmux -cz -f "${target:?}"/share/tmuxcfg.tar.gz ${tmuxcfg:?}
-wget -N -q -O "${target:?}"/dist/tmux.tar.gz ${tmux:?}
-wget -N -q -O "${target:?}"/dist/bat.tar.gz ${bat:?} 
-wget -N -q -O "${target:?}"/dist/lsd.tar.gz ${lsd:?}
-wget -N -q -O "${target:?}"/dist/procs.zip ${procs:?}
-wget -N -q -O "${target:?}"/dist/delta.tar.gz ${delta:?}
-wget -N -q -O "${target:?}"/dist/micro.tar.gz ${micro:?}
-wget -N -q -O "${target:?}"/dist/hl.tar.gz ${hl:?}
+download "${tmux:?}" "${target:?}"/dist/tmux.tar.gz
+download "${bat:?}"  "${target:?}"/dist/bat.tar.gz
+download "${lsd:?}" "${target:?}"/dist/lsd.tar.gz
+download "${procs:?}" "${target:?}"/dist/procs.zip
+download "${delta:?}" "${target:?}"/dist/delta.tar.gz
+download "${micro:?}" "${target:?}"/dist/micro.tar.gz
+download "${hl:?}" "${target:?}"/dist/hl.tar.gz
